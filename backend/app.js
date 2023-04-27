06b666641fac14f3931157fc09975a92dacf5735
Mizba-Anjum <89572205+Mizba-Anjum@users.noreply.github.com>
2023-04-27 17:53:13 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,16 @@ const app = express()
 
 // add cors header to the server
 app.use(
+  function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+},
   cors({
     origin: '*'
   })
-)
+);
 
 // sets up mongoose for the mongoDB connection
 mongoose
